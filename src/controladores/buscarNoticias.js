3be0ee864470e3d30cf618be8dc68921a7cb3b4c
Vidal-Gus <vidal47pro@gmail.com>
2023-09-21 14:45:50 -0300

    trocando top-headlines por everything

--- src/controladores/buscarNoticias.js
+++ src/controladores/buscarNoticias.js
@@ -4,16 +4,18 @@ const axios = require('axios');
 const urlCEP = 'https://brasilapi.com.br/api/cep/v1/'
 
 const apiKey = 'apiKey=59a33bcca8a84ed8aa0c8a22089faa51'
-const urlNoticia = `https://newsapi.org/v2/top-headlines?`
+const urlNoticia = `https://newsapi.org/v2/everything?`
 const buscarNoticias = {
     buscarPorCep: async function (req, res) {
         try {
             const { cep } = req.query;
             const enderecoObj = await axios.get(urlCEP + cep);
             const { state, city, neithborhood, street } = enderecoObj.data;
-            const palavrasChave = `q=${city}`;
+            const palavrasChave = `q=${street}`;
 
-            const noticias = await axios.get(`${urlNoticia}country=br&${palavrasChave}&${apiKey}`);
+            //const noticias = await axios.get(`${urlNoticia}country=br&${palavrasChave}&${apiKey}`);
+            const noticias = await axios.get(`${urlNoticia}${palavrasChave}&sortBy=popularity&language=pt&${apiKey}`);
+            console.log(enderecoObj.data)
             return res.status(200).json(noticias.data);
 
         } catch (e) {
